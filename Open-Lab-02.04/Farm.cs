be6ea1dab087fbde7590ea12a12540487278459e
Richardjurco <113421214+Richardjurco@users.noreply.github.com>
2022-09-26 12:08:44 +0200

    Update Farm.cs

--- Open-Lab-02.04/Farm.cs
+++ Open-Lab-02.04/Farm.cs
@@ -1,12 +1,15 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_02._04
 {
     class Farm
     {
-        public int GetLegsCount(int chickens, int cows, int pigs)
+        public int GetLegsCount(int chickens , int cows , int pigs )
         {
+            return chickens * 2 + (cows + pigs) * 4;
+
             throw new NotImplementedException();
+
         }
     }
 }
